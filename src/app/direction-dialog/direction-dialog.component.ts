3836c98df61c0565fb26a7a7a60bd1f451632e14
Tarcisio Guimarães <79273548+targdev@users.noreply.github.com>
2024-06-08 12:49:40 -0300

    change link ios download app

--- src/app/direction-dialog/direction-dialog.component.ts
+++ src/app/direction-dialog/direction-dialog.component.ts
@@ -3,16 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-direction-dialog',
   templateUrl: './direction-dialog.component.html',
-  styleUrls: ['./direction-dialog.component.scss']
+  styleUrls: ['./direction-dialog.component.scss'],
 })
 export class DirectionDialogComponent {
-  isBlank: string = "_blank";
+  isBlank: string = '_blank';
 
-  urlAppBaberAppStore: string = "https://apps.apple.com/app/id1079972583";
-  appStore: string = "AppStore";
-  imgAppStore: string = "../../assets/appstore.png";
+  urlAppBaberAppStore: string =
+    'https://apps.apple.com/us/app/appbarber-pro-profissionais/id1602535418?ign-itscg=30200&ign-itsct=apps_box_badge';
+  appStore: string = 'AppStore';
+  imgAppStore: string = '../../assets/appstore.png';
 
-  urlAppBaberPlayStore: string = "https://play.google.com/store/apps/details?id=com.startapp.appbarber";
-  playStore: string = "PlayStore";
-  imgPlayStore: string = "../../assets/playstore.png";
+  urlAppBaberPlayStore: string =
+    'https://play.google.com/store/apps/details?id=com.startapp.appbarber';
+  playStore: string = 'PlayStore';
+  imgPlayStore: string = '../../assets/playstore.png';
 }
